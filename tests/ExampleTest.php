ca1af41f5d2adb201d742ec935c379b7ebfba49c
enricodelazzari <enrico.delazzari@h-farm.com>
2023-02-28 09:26:35 +0100

    wip

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,3 +13,15 @@ it('can test', function ($document, $result) {
     ['document' => '/Support/XML/1/document.xml', 'result' => '/Support/XML/1/result.xml'],
     ['document' => '/Support/XML/2/document.xml', 'result' => '/Support/XML/2/result.xml'],
 ]);
+
+
+it('can test1', function ($document, $result) {
+
+    $data = Processor::fromFilename(__DIR__ . $document)->execute();
+
+    expect($data)->toEqual(
+        file_get_contents(__DIR__ . $result)
+    );
+})->with([
+    ['document' => '/Support/XML/4/document.xml', 'result' => '/Support/XML/4/result.xml'],
+]);
